895e609a4fd5efdb4f2a8dada47e9aee4fca2827
Александр Герасимов <gesya@internet.ru>
2024-09-11 11:32:50 +0700

    Вывод страницы первой лабораторной

--- app.py
+++ app.py
@@ -3,4 +3,22 @@ app = Flask(__name__)
 
 @app.route("/")
 def start():
-    return "web-сервер на flask"
+    return """
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Герасимов Александр Александрович, лабораторная работа 1</title>
+    </head>
+    <body>
+        <header>
+            НГТУ, ФБ, Лабораторная работа 1
+        </header>
+
+        <h1>web-сервер на flask</h1>
+
+        <footer>
+            &copy; Александр Герасимов, ФБИ-23, 3 курс, 2024
+        </footer>
+    </body>
+</html>
+"""
