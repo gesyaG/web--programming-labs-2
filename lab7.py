e74eb73b7c5a971dfa9db48cb2440b27eecb70c8
Alexander Gerasimov <gesya@internet.ru>
2024-12-23 19:46:01 +0700

    Удаление фильма по REST

--- lab7.py
+++ lab7.py
@@ -53,3 +53,12 @@ def get_film(id):
         return films[id]
     else:
         return {'error': 'Film not found'}, 404
+
+
+@lab7.route('/lab7/rest-api/films/<int:id>', methods=['DELETE'])
+def del_film(id):
+    if 0 <= id < len(films):
+        del films[id]
+        return '', 204
+    else:
+        return {'error': 'Film not found'}, 404
