60a87587ef80a0969e43f5d0e62bfe50caf55f09
Александр Герасимов <gesya@internet.ru>
2024-12-27 22:03:31 +0700

    Модель базы данных

--- lab8.py
+++ lab8.py
@@ -1,6 +1,6 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request, redirect, session, flash, current_app, url_for
 
-lab7 = Blueprint('lab8', __name__)
+lab8 = Blueprint('lab8', __name__)
 
 
 @lab8.route('/lab8/')
