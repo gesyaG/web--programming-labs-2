fdc8a530566f6cba202b9d66b9fbbc6d3f2bf5c9
Александр Герасимов <gesya@internet.ru>
2024-12-27 22:43:43 +0700

    Заготовка к 9-ой лабораторной

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ from lab5 import lab5
 from lab6 import lab6
 from lab7 import lab7
 from lab8 import lab8
+from lab8 import lab9
 from RGZ import RGZ
 
 app = Flask(__name__)
@@ -54,6 +55,7 @@ app.register_blueprint(lab5)
 app.register_blueprint(lab6)
 app.register_blueprint(lab7)
 app.register_blueprint(lab8)
+app.register_blueprint(lab9)
 app.register_blueprint(RGZ)
 
 
@@ -82,6 +84,7 @@ def menu():
         <a href="/lab6">Шестая лабораторная</a>
         <a href="/lab7">Седьмая лабораторная</a>
         <a href="/lab8">Восьмая лабораторная</a>
+        <a href="/lab9">Девятая лабораторная</a>
         <a href="/RGZ/login">РГЗ</a>
 
         <footer>
