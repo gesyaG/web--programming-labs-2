4990a99c58b20c35f81e3023b88460fedeefe2ad
Александр Герасимов <gesya@internet.ru>
2024-12-27 22:16:44 +0700

    Модель базы данных еще раз

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ if app.config['DB_TYPE'] == 'postgres':
 
 else:
     dir_path = path.dirname(path.realpath(__file__))
-    db_path = path.join(dir_path, "timur.db")
+    db_path = path.join(dir_path, "alexander_gerasimov_orm.db")
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_path}'
 
 db.init_app(app)
